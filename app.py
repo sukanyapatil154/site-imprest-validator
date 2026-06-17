266e1a2ee0d820d5a1870be50b27403689694898
sukanyapatil154 <sukanya.mulkipatil@envision-energy.com>
2026-06-17 09:57:43 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from utils.validator import validate
 
 st.set_page_config(layout="wide")
 
-st.title("Site Imprest ")
+st.title("Site Imprest a ")
 
 uploaded_pdf = st.file_uploader(
     "Upload Site Imprest PDF",
